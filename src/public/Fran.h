84d9dd237cfba616e9cacbe784cc22765cb684ff
Mia <miasedgewick@gmail.com>
2026-08-31 12:29:40 -0700

    Added Fran
    module already made a year ago for an animation tool, only has behaviours implemented currently

---
+++ src/public/Fran.h
@@ -0,0 +1,30 @@
+#include <functional>
+
+// Functional Reactive Animation
+namespace FRAN
+{
+	// A function of time returning any real number
+	template <typename T>
+	using Behavior = std::function<T(float)>;
+
+	// A Behavior which does not change its value over time
+	template <typename T>
+	Behavior<T> Constant(const T& Value)
+	{
+		return [Value](float) { return Value; };
+	}
+
+	// Identity time Behavior
+	//inline TBehavior<float> Time = [](float Time) {return Time;};
+
+	// The Lifting function turns all parameters of a function into Behaviors
+	template <typename F, typename... B>
+	auto Lift(F Func, B... Behaviors)
+	{
+		return [=](float Time)
+		{
+			return Func(Behaviors(Time)...);
+		};
+	}
+}
+
